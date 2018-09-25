ba723f9d4828eb868b3af17d758938fb49b6eb3c
simondr <simodraxl@tsn.at>
2018-09-25 09:10:28 +0200

    Bubblesort working

--- Bubblesort/src/Main.java
+++ Bubblesort/src/Main.java
@@ -8,12 +8,34 @@ public class Main {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
         Random r = new Random();
 
-        for(int i = 0; i<= 9; i++){
+        //fills array with random integers and prints array
+        for (int i = 0; i <= 9; i++) {
 
             arrayList.add(r.nextInt(100));
             System.out.println(arrayList.get(i));
         }
 
+        System.out.println();
+
+        //bubblesort
+        for (int i = 0; i <= 9; i++) {
+
+            for (int j = 1; j <= 9; j++) {
+
+                if (arrayList.get(j - 1) > arrayList.get(j)) {
+                    int a = arrayList.get(j - 1);
+                    arrayList.set(j - 1, arrayList.get(j));
+                    arrayList.set(j, a);
+                }
+            }
+        }
+
+        //prints sorted array
+        for (int i = 0; i <= 9; i++) {
+
+            arrayList.add(r.nextInt(100));
+            System.out.println(arrayList.get(i));
+        }
 
     }
 }
