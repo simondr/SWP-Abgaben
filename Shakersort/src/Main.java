a7882379d0b6ef889cbef90b908baeca8747b4de
simondr <simodraxl@tsn.at>
2018-09-27 16:49:24 +0200

    shakersort working

--- Shakersort/src/Main.java
+++ Shakersort/src/Main.java
@@ -5,45 +5,67 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        Random r = new Random();
 
-        //fills array with random integers and prints it
-        for (int i = 0; i <= 9; i++) {
-            arrayList.add(r.nextInt(100));
+        long starttime = System.nanoTime();
+        long elapsedtime = 0;
+        ArrayList<Integer> arrayList = getRandomList(100);
+
+        for (int i = 0; i <= arrayList.size() - 1; i++) {
             System.out.println(arrayList.get(i));
         }
 
         System.out.println();
 
-        int maxindex = 9;
+        int maxindex = arrayList.size() - 1;
         int minindex = 0;
 
         while (minindex < maxindex) {
-            for (int i = 0; i <= maxindex; i++) {
+            //
+            for (int i = 0; i < maxindex; i++) {
 
-                if (arrayList.get(i + 1) > arrayList.get(i)) {
-                    maxindex = i;
+                if (arrayList.get(i + 1) < arrayList.get(i)) {
+
                     int a = arrayList.get(i + 1);
                     arrayList.set(i + 1, arrayList.get(i));
                     arrayList.set(i, a);
+
                 }
 
             }
+            maxindex--;
 
-           for (int i = 9; i >= minindex; i--) {
+            for (int i = 9; i > minindex; i--) {
 
-                if (arrayList.get(i-1) < arrayList.get(i)) {
-                    minindex = i;
+                if (arrayList.get(i - 1) > arrayList.get(i)) {
+
                     int a = arrayList.get(i - 1);
                     arrayList.set(i - 1, arrayList.get(i));
                     arrayList.set(i, a);
                 }
             }
+            minindex++;
         }
 
-        for (int i = 0; i <= 9; i++) {
+
+        for (int i = 0; i <= arrayList.size() - 1; i++) {
             System.out.println(arrayList.get(i));
         }
+
+
+        elapsedtime = System.nanoTime() - starttime;
+        System.out.println("elapsed time " + elapsedtime + " ns");
+
+    }
+
+    public static ArrayList<Integer> getRandomList(int size) {
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        Random r = new Random();
+        for (int i = 0; i <= size - 1; i++) {
+
+            arrayList.add(r.nextInt(100));
+        }
+        return arrayList;
+
     }
 }
