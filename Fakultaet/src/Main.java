0414c0145773a49485781ace856a0e31e71d27be
simondr <simodraxl@tsn.at>
2018-10-04 16:21:26 +0200

    fibonacci added, iterative Pascal Triangle not working

--- Fakultaet/src/Main.java
+++ Fakultaet/src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         System.out.println(facRecursive(5));
 
         System.out.println(isPalindromRecursive("anna"));
-
+        System.out.println(pascalTrinagleRecursive(10, 5));
+        //System.out.println(pascalTriangle(10, 5));
+        System.out.println(fibonacci(5));
     }
 
     public static int fac(int n) {
@@ -16,15 +18,15 @@ public class Main {
         return result;
     }
 
-    public static int facRecursive(int n){
-        if (n==1)
+    public static int facRecursive(int n) {
+        if (n == 1)
             return 1;
-        return n * fac(n-1);
+        return n * fac(n - 1);
     }
 
     public static boolean isPalindrom(String s) {
         String string = "";
-        for (int i = s.length()-1; i >= 0; i--) {
+        for (int i = s.length() - 1; i >= 0; i--) {
             string += s.charAt(i);
         }
         if (s.equals(string))
@@ -33,15 +35,49 @@ public class Main {
             return false;
     }
 
-    public static boolean isPalindromRecursive(String s){
-          if (s.length() == 1)
-              return true;
-          if(s.length() == 2 && s.charAt(0) == s.charAt(1))
-              return true;
-          if(s.charAt(0) == s.charAt(s.length()-1)) {
+    public static boolean isPalindromRecursive(String s) {
+        if (s.length() == 1)
+            return true;
+        if (s.length() == 2 && s.charAt(0) == s.charAt(1))
+            return true;
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
 
-              return isPalindrom(s.substring(1,s.length()-2));
-          }
-          return false;
+            return isPalindrom(s.substring(1, s.length() - 2));
+        }
+        return false;
+    }
+
+
+    public static int pascalTrinagleRecursive(int x, int y) {
+
+        if (x == y || y == 0)
+            return 1;
+
+        return pascalTrinagleRecursive(x - 1, y - 1) + pascalTrinagleRecursive(x-1,y);
+    }
+
+    public static int pascalTriangle(int x, int y){
+
+        int[][] pascalTriangle = new int[x][y];
+        pascalTriangle[0][0] = 1;
+        for (int i = 1; i <= x; i++){
+            for (int j = 1; j<= y; j++){
+                pascalTriangle[x][y] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+
+
+            }
+        }
+        return pascalTriangle[x] [y];
+
+    }
+
+    public static int fibonacci(int index){
+
+        if(index == 0)
+            return 1;
+        if (index == -1)
+            return 1;
+        return fibonacci(index-1) + fibonacci(index-2);
+
     }
 }
