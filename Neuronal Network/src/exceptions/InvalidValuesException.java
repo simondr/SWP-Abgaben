1ccf87e380f2f4afc0874a3999da192f4249a0da
simondr <simodraxl@tsn.at>
2019-06-04 09:04:25 +0200

    first version; identical to project Matrix Calculator

---
+++ Neuronal Network/src/exceptions/InvalidValuesException.java
@@ -0,0 +1,4 @@
+package exceptions;
+
+public class InvalidValuesException extends Exception {
+}
---
+++ Neuronal Network/src/neuronal_network/ArtificialNeuronalNetwork.java
@@ -0,0 +1,21 @@
+package neuronal_network;
+
+import matirx.Matrix;
+import java.util.ArrayList;
+
+public class ArtificialNeuronalNetwork {
+
+    private int inputneurons;
+    private int[] hiddenlayers;
+    private int outputneurons;
+    private ArrayList<Matrix> weights;
+    private ArrayList<Matrix> biases;
+
+
+    public ArtificialNeuronalNetwork(int inputneurons, int[] hiddenlayers, int outputneurons) {
+        this.inputneurons = inputneurons;
+        this.hiddenlayers = hiddenlayers;
+        this.outputneurons = outputneurons;
+    }
+
+}
